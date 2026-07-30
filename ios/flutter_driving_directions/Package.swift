27e26e0691f4cf5a350db62bcf9f59709ac19619
joe <joe.blough@gmail.com>
2026-07-30 14:59:38 -0400

    Migrate to the latest Flutter, Gradle, and Swift Package Manager

---
+++ ios/flutter_driving_directions/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "flutter_driving_directions",
+    platforms: [
+        .iOS("13.0")
+    ],
+    products: [
+        .library(name: "flutter-driving-directions", targets: ["flutter_driving_directions"])
+    ],
+    dependencies: [
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
+    ],
+    targets: [
+        .target(
+            name: "flutter_driving_directions",
+            dependencies: [
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ],
+            resources: []
+        )
+    ]
+)
